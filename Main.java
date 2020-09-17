687078456a907cfcad58312e6582dec0231861d4
WindMS <alfredxuedreams@gmail.com>
2020-09-17 21:03:00 +0000

    Small code clean up, fixed handling gloves with less than 15 att

--- Main.java
+++ Main.java
@@ -1,44 +1,60 @@
 import java.util.Scanner;
-
 class Main {
   private static boolean getAnswer(Scanner scan) {
-       while (true) {
-        switch (scan.nextLine().trim().toLowerCase()) {
-        case "y":
-          return true;
-        case "n":
-          return false;
-        default:
-          System.out.println("Invalid Input Detected! Please enter y/n: ");
-          break;
-        }
-       }
+    while (true) {
+      switch (scan.nextLine().trim().toLowerCase()) {
+      case "y":
+        return true;
+      case "n":
+        return false;
+      default:
+        System.out.println("Invalid Input Detected! Please enter y/n: ");
+        break;
+      }
+    }
+  }
+
+  private static String getScrollType(Scanner scan) {
+    while (true) {
+      String scrollType = scan.nextLine().trim().toLowerCase();
+      switch (scrollType) {
+      case "ags":
+      case "ss":
+      case "prime":
+        return scrollType;
+      default:
+        System.out.println("Invalid Input Detected! Please enter ags,ss or prime: ");
+        break;
+      }
+    }
   }
 
   public static void main(String[] args) {
-    //sf stat table
-    Integer[] sfTyrantStat = new Integer[] {0,18,38,60,84,110,110, 110,110,110,110,110,110,110,110,110};
-    Integer[] sfTyrantAtt = new Integer[] {0,0,0,0,0,0,8,17,27,38,50,63,77,92,108,125};
-    Integer[] sfArcaneStat = new Integer[] {0,2,4,6,8,10,13,16,19,22,25,28,31,34,37,40,55,70,85,100,115,130};
-    Integer[] sfArcaneAtt = new Integer[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,14,29,45,62,80,99};
-    Integer[] sfSweetWaterStat = new Integer[] {0,2,4,6,8,10,13,16,19,22,25,28,31,34,37,53,66,79,92,105,118};
-    Integer[] sfSweetWaterAtt = new Integer[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11,23,36,50,65,81};
-    Integer[] sfSuperiorStat = new Integer[] {0,2,4,6,8,10,13,16,19,22,25,28,31,34,37,40,51,62,73,84,95,106};
-    Integer[] sfSuperiorAtt = new Integer[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,21,33,46,60,75};
-    Integer[] sfReinforcedStat = new Integer[] {0,2,4,6,8,10,13,16,19,22,25,28,31,34,37,40,49,58,67,76,85,94};
-    Integer[] sfReinforcedAtt = new Integer[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,19,30,42,55,69};
-    Integer[] sfSolidStat = new Integer[] {0,2,4,6,8,10,13,16,19,22,25,28,31,34,37,40,47,54,61,68,75};
-    Integer[] sfSolidAtt = new Integer[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,17,27,38,50};
+    // sf stat table
+    Integer[] sfTyrantStat = new Integer[] { 0, 18, 38, 60, 84, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110, 110 };
+    Integer[] sfTyrantAtt = new Integer[] { 0, 0, 0, 0, 0, 0, 8, 17, 27, 38, 50, 63, 77, 92, 108, 125 };
+    Integer[] sfArcaneStat = new Integer[] { 0, 2, 4, 6, 8, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 55, 70, 85, 100,
+        115, 130 };
+    Integer[] sfArcaneAtt = new Integer[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 29, 45, 62, 80, 99 };
+    Integer[] sfSweetWaterStat = new Integer[] { 0, 2, 4, 6, 8, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 53, 66, 79, 92,
+        105, 118 };
+    Integer[] sfSweetWaterAtt = new Integer[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 23, 36, 50, 65,
+        81 };
+    Integer[] sfSuperiorStat = new Integer[] { 0, 2, 4, 6, 8, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 51, 62, 73,
+        84, 95, 106 };
+    Integer[] sfSuperiorAtt = new Integer[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 21, 33, 46, 60, 75 };
+    Integer[] sfReinforcedStat = new Integer[] { 0, 2, 4, 6, 8, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 49, 58, 67,
+        76, 85, 94 };
+    Integer[] sfReinforcedAtt = new Integer[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 19, 30, 42, 55, 69 };
+    Integer[] sfSolidStat = new Integer[] { 0, 2, 4, 6, 8, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 47, 54, 61, 68,
+        75 };
+    Integer[] sfSolidAtt = new Integer[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 17, 27, 38, 50 };
     // Welcome dialog
     System.out.println("Welcome to Halcyon's MCS Calculator\n");
 
     while (true) {
       // Variable Declaration
       Scanner scan = new Scanner(System.in);
-      String ssConst = "ss";
-      String agsConst = "ags";
-      String primeConst = "prime";
-      String scrollType = "";
       Integer scrollStatValue = 0;
       Integer scrollAttValue = 0;
       boolean isGlove = false;
@@ -58,7 +74,7 @@ class Main {
         // Check if the item is a Tyrant
         System.out.println("Is this item a Tyrant? Please enter y/n: ");
         isTyrant = getAnswer(scan);
-        if (!isTyrant) {
+        if (!isTyrant && !isGlove) {
           // Check if the item is a Ghost Ship Badge
           System.out.println("Is this item a Ghost Ship Badge? Please enter y/n: ");
           isGhostShip = getAnswer(scan);
@@ -68,45 +84,25 @@ class Main {
       // Get Type of Scroll Used
       if (!isGhostShip) {
         System.out.println("How is this item scrolled? Please enter ags,ss,or prime.");
-        // This lives for another day cuz I'm lazy
-        boolean quit = false;
-        while (!quit) {
-          scrollType = scan.nextLine().trim().toLowerCase();
-          switch (scrollType) {
-          case "ags":
-          case "ss":
-          case "prime":
-            quit = true;
-            break;
-          default:
-            System.out.println("Invalid Input Detected! Please enter ags,ss or prime: ");
-            break;
-          }
-        }
-
         // Calculate added stats from scrolls
-        if (scrollType.equals(agsConst)) {
+        switch (getScrollType(scan)) {
+        case "ags":
           scrollStatValue = 3;
           scrollAttValue = 4;
-        }
-
-        else if (scrollType.equals(ssConst) && isGlove == true) {
-          scrollStatValue = 0;
-          scrollAttValue = 4;
-        }
-
-        else if (isGhostShip) {
-          scrollStatValue = 0;
-          scrollAttValue = 0;
-        }
-
-        else if (scrollType.equals(primeConst)) {
+          break;
+        case "ss":
+          if (isGlove) {
+            scrollStatValue = 0;
+            scrollAttValue = 4;
+          } else {
+            scrollStatValue = 9;
+            scrollAttValue = 0;
+          }
+          break;
+        case "prime":
           scrollStatValue = 10;
-        }
-
-        else {
-          scrollStatValue = 9;
-          scrollAttValue = 0;
+          break;
+        default:
         }
       }
 
@@ -142,7 +138,7 @@ class Main {
       if (itemLevel >= 150 && itemLevel <= 159 && !isTyrant) {
         addedStatValue -= sfSuperiorStat[currentStars];
         if (!isGhostShip) {
-        addedAttValue -= sfSuperiorAtt[currentStars];
+          addedAttValue -= sfSuperiorAtt[currentStars];
         }
       }
 
@@ -164,17 +160,19 @@ class Main {
         addedAttValue -= sfArcaneAtt[currentStars];
       }
 
-      // In AriesMS,Gloves gain 7 ATT when SF'd from 1-15
-      if (isGlove == true)
-        addedAttValue = addedAttValue - 7;
+      // In AriesMS,Gloves gain 7 ATT when SF'd from 1-15,
+      // gaining att at 1,3,5,7,11,13. 
+      if (isGlove == true) {
+        int extraGloveAtt = (Math.min(currentStars, 13) + 1) / 2;
+        addedAttValue = addedAttValue - extraGloveAtt;
+      }
 
       System.out.println("\nCalculating MCS..");
       System.out.println("Minimum MCS Combination found: ");
 
       if (isGhostShip) {
         System.out.println(totalScrollCount + " Slot MCS Found: STAT: " + addedStatValue + ",WA: +" + addedAttValue);
-      }
-      else {
+      } else {
         int testSlotScenario = totalScrollCount;
 
         for (int i = 1; i <= 90; i++) {
